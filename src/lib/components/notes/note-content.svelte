b2558eba538b83c0acec709df49163391937f451
Dallas Hoffman <dallashoff@gmail.com>
2023-10-03 01:01:43 -0400

    Add bytemd gfm plugin

--- src/lib/components/notes/note-content.svelte
+++ src/lib/components/notes/note-content.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import { Editor, Viewer } from 'bytemd';
+	import gfmPlugin from '@bytemd/plugin-gfm';
 	import type { Note } from '$lib/stores/notes';
 	import type { NoteEditorStore } from '$lib/stores/note-editor';
 
 	export let note: Note;
 	export let noteEditorStore: NoteEditorStore;
 	const { editing, draftContent, draftChange } = noteEditorStore;
+
+	const plugins = [gfmPlugin()];
 </script>
 
 <div class="note-content" data-theme="light">
 	{#if $editing}
 		<div class="note-content__editor">
-			<Editor value={$draftContent} on:change={draftChange} />
+			<Editor value={$draftContent} {plugins} on:change={draftChange} />
 		</div>
 	{:else}
 		<div class="note-content__viewer">
-			<Viewer value={note.content} />
+			<Viewer value={note.content} {plugins} />
 		</div>
 	{/if}
 </div>
