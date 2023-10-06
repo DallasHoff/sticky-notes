e25f3001fa925353cbdc07afa853f065c808136d
Dallas Hoffman <dallashoff@gmail.com>
2023-10-05 23:21:25 -0400

    Add web app manifest and splash images

---
+++ pwa-assets.config.ts
@@ -0,0 +1,36 @@
+import {
+	AllAppleDeviceNames,
+	createAppleSplashScreens,
+	defineConfig,
+} from '@vite-pwa/assets-generator/config';
+
+export default defineConfig({
+	images: ['static/favicon.svg'],
+	preset: {
+		transparent: { sizes: [] },
+		maskable: { sizes: [] },
+		apple: { sizes: [] },
+		appleSplashScreens: createAppleSplashScreens(
+			{
+				name: (landscape, size, dark) => {
+					return `apple-splash-${landscape ? 'landscape' : 'portrait'}-${
+						typeof dark === 'boolean' ? (dark ? 'dark-' : 'light-') : ''
+					}${size.width}x${size.height}.png`;
+				},
+				padding: 0.8,
+				resizeOptions: { background: '#fbfbfc', fit: 'contain' },
+				darkResizeOptions: { background: '#18232c', fit: 'contain' },
+				linkMediaOptions: {
+					basePath: '/',
+					log: true,
+					addMediaScreen: false,
+				},
+				png: {
+					compressionLevel: 9,
+					quality: 60,
+				},
+			},
+			[...AllAppleDeviceNames]
+		),
+	},
+});
