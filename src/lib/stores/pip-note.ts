da52aad8e0d3644a4e1e126f61d7d2b0a7d6b1c8
Dallas Hoffman <dallashoff@gmail.com>
2025-11-04 23:43:05 -0500

    Update popped-out note when it is edited in the main window

--- src/lib/stores/pip-note.ts
+++ src/lib/stores/pip-note.ts
@@ -1,6 +1,7 @@
 import NoteCard from '$lib/components/notes/note-card.svelte';
 import { writable, type Readable } from 'svelte/store';
 import type { Note } from './notes';
+import { notes } from '.';
 
 export class PipNoteStore implements Readable<Note | null> {
 	private activeNote = writable<Note | null>(null);
@@ -8,26 +9,48 @@ export class PipNoteStore implements Readable<Note | null> {
 	subscribe = this.activeNote.subscribe;
 
 	async openPipNoteCard(note: Note) {
-		// Create an element and mount a note component into it
-		const target = document.createElement('div');
-		target.id = 'picture-in-picture';
-
-		new NoteCard({ target, props: { note, isReadonly: true } });
-
 		// Create picture-in-picture window
 		// @ts-expect-error documentPictureInPicture is experimental
 		const pipWindow = await window.documentPictureInPicture.requestWindow({
 			width: 355,
 			height: 385,
 		});
+		this.copyAllStylesheets(pipWindow);
+
+		// Update the note when it is edited in the main window
+		const unsubscribe = notes.subscribe((updatedNotes) => {
+			const updatedNote = updatedNotes.find((n) => n.id === note.id);
+			if (!updatedNote) return;
+			this.mountNote(pipWindow, updatedNote);
+		});
 
 		// Handle clearing active note when the picture-in-picture window closes
-		pipWindow.addEventListener('pagehide', () => this.activeNote.set(null), { once: true });
+		pipWindow.addEventListener(
+			'pagehide',
+			() => {
+				unsubscribe();
+				this.activeNote.set(null);
+			},
+			{ once: true }
+		);
+	}
+
+	private mountNote(pipWindow: Window, note: Note) {
+		// Create a wrapper element
+		const target = document.createElement('div');
+		target.id = 'picture-in-picture';
+
+		// Mount the note card component
+		new NoteCard({ target, props: { note, isReadonly: true } });
+
+		// Put it into the window
+		pipWindow.document.body.replaceChildren(target);
 
 		// Update the active note
 		this.activeNote.set(note);
+	}
 
-		// Copy all stylesheets
+	private copyAllStylesheets(pipWindow: Window) {
 		[...document.styleSheets].forEach((styleSheet) => {
 			try {
 				const cssRules = [...styleSheet.cssRules].map((rule) => rule.cssText).join('');
@@ -45,8 +68,5 @@ export class PipNoteStore implements Readable<Note | null> {
 				pipWindow.document.head.appendChild(link);
 			}
 		});
-
-		// Add the element to the picture-in-picture window
-		pipWindow.document.body.append(target);
 	}
 }
